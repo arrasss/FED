ebcd04dac2e8f9ec3af4708cc778201a5f2f3665
arrasss <144111759+arrasss@users.noreply.github.com>
2025-09-30 19:22:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,17 @@
 // JavaScript Document
 console.log("hi");
+
+let menuButton = document.querySelector("header button");
+let hetMenu = document.querySelector("header nav");
+let sluitButton = document.querySelector("header nav button");
+
+menuButton.addEventListener("click", openMenu);
+sluitButton.addEventListener("click", sluitMenu);
+
+function openMenu() {
+  hetMenu.classList.add("toonMenu");
+}
+
+function sluitMenu() {
+  hetMenu.classList.remove("toonMenu");
+}
